0e520fa194e45c486873ee65de72cc8582e81436
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-04-28 00:02:04 +0200

    Update AppTest.java

--- src/test/java/com/uregina/app/AppTest.java
+++ src/test/java/com/uregina/app/AppTest.java
@@ -135,7 +135,95 @@ public class AppTest
             System.out.println("Time");
         }
     }
+	
+	
+    @Test
+    public void Subtract_Invalid_h1_1()
+    {
+    	try
+        {
+	    	Time12 time121 = new Time12(-25, 1, AmPm.pm);
+		Time12 time122 = new Time12(-25, -2, AmPm.pm);
+	    	int answer = subtract(time121, time122);
+		
+	        assertTrue(actualMessage.contains("Invalid Time"));
+	    }
+	    catch(InvalidTimeException e)
+        {
+            System.out.println("Time");
+        }
+    }
 
+	
+   @Test
+    public void Subtract_h2_moreThan_h1()
+    {
+    	try
+        {
+	    	Time12 time121 = new Time12( 1, 2, AmPm.pm);
+		Time12 time122 = new Time12( 2, 2, AmPm.pm);
+	    	int answer = subtract(time121, time122);
+		
+	        assertEquals( -60, answer);
+	    }
+	    catch(InvalidTimeException e)
+        {
+            System.out.println("Time");
+        }
+    }
+	
+   @Test
+    public void Subtract_h1_moreThan_h2()
+    {
+    	try
+        {
+	    	Time12 time121 = new Time12( 2, 2, AmPm.pm);
+		Time12 time122 = new Time12( 1, 2, AmPm.pm);
+	    	int answer = subtract(time121, time122);
+		
+	        assertEquals( 60, answer);
+	    }
+	    catch(InvalidTimeException e)
+        {
+            System.out.println("Time");
+        }
+    }
+	
+	
+   @Test
+    public void Subtract_m2_moreThan_m1()
+    {
+    	try
+        {
+	    	Time12 time121 = new Time12( 1, 1, AmPm.pm);
+		Time12 time122 = new Time12( 1, 2, AmPm.pm);
+	    	int answer = subtract(time121, time122);
+		
+	        assertEquals( -1, answer);
+	    }
+	    catch(InvalidTimeException e)
+        {
+            System.out.println("Time");
+        }
+    }
+	
+	
+   @Test
+    public void Subtract_m1_moreThan_m1()
+    {
+    	try
+        {
+	    	Time12 time121 = new Time12( 1, 2, AmPm.pm);
+		Time12 time122 = new Time12( 1, 1, AmPm.pm);
+	    	int answer = subtract(time121, time122);
+		
+	        assertEquals( 1, answer);
+	    }
+	    catch(InvalidTimeException e)
+        {
+            System.out.println("Time");
+        }
+    }
 }
 
 
